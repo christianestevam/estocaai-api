dea2e798960dcec73fb421c80db87f9d141dbd10
Natan Lucena <zerogamerptbr@gmail.com>
2024-09-21 11:15:41 -0300

    feat: setup to jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { SignUpUserService } from './services/sign-up-user/sign-up-user.service';
 import { SignUpUserController } from './controllers/sign-up-user/sign-up-user.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SignInUserService } from './services/sign-in-user/sign-in-user.service';
+import { SignTokenService } from './services/sign-token/sign-token.service';
+import { SignInUserController } from './controllers/sign-in-user/sign-in-user.controller';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [PrismaService, SignUpUserService],
-  controllers: [SignUpUserController],
+  providers: [
+    JwtService,
+    PrismaService,
+    SignUpUserService,
+    SignInUserService,
+    SignTokenService,
+  ],
+  controllers: [SignUpUserController, SignInUserController],
 })
 export class AuthModule {}
